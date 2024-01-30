f0c677739a57053b0613225fd6500ead1d84d99a
AlexandreUff <alexandre_magno@id.uff.br>
2024-01-30 14:53:08 -0300

    feat: method to make rail run

--- src/app/component/carousel-input/carousel-input.component.ts
+++ src/app/component/carousel-input/carousel-input.component.ts
@@ -1,12 +1,20 @@
+import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-carousel-input',
   standalone: true,
-  imports: [],
+  imports: [NgStyle],
   templateUrl: './carousel-input.component.html',
   styleUrl: './carousel-input.component.scss'
 })
 export class CarouselInputComponent {
+  valor = 5
+  public railPosition: number = 0
+
+  public changeRailPosition = () => {
+    this.railPosition--
+    console.log("Posicionista:", this.railPosition)
+  }
 
 }
