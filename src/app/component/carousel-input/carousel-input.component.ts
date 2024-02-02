55e6d3507fe6db7a312e383d41b844f563c40686
AlexandreUff <alexandre_magno@id.uff.br>
2024-02-02 16:02:15 -0300

    set select element and its styles

--- src/app/component/carousel-input/carousel-input.component.ts
+++ src/app/component/carousel-input/carousel-input.component.ts
@@ -1,15 +1,34 @@
 import { NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+interface userDataType {
+  weight: number,
+  height: number,
+  age: number,
+  activity: number,
+  sex: "male" | "female",
+  objective: "hypertrophy" | "loseweight"
+}
 
 @Component({
   selector: 'app-carousel-input',
   standalone: true,
-  imports: [NgStyle],
+  imports: [NgStyle, FormsModule],
   templateUrl: './carousel-input.component.html',
   styleUrl: './carousel-input.component.scss'
 })
 export class CarouselInputComponent {
   public railPosition: number = 0
+
+  public userData: userDataType = {
+    weight: 0,
+    height: 0,
+    age: 0,
+    activity: 0,
+    sex: "male",
+    objective: "hypertrophy"
+  }
 
   public changeRailPosition = (advance: boolean) => {
     const newValue = advance ? this.railPosition - 1 : this.railPosition + 1
