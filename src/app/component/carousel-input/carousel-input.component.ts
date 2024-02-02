ec21842b99a6982adf556ab23db8ef1119195147
AlexandreUff <alexandre_magno@id.uff.br>
2024-02-02 15:27:26 -0300

    feat: add disabled to done button

--- src/app/component/carousel-input/carousel-input.component.ts
+++ src/app/component/carousel-input/carousel-input.component.ts
@@ -13,8 +13,12 @@ export class CarouselInputComponent {
 
   public changeRailPosition = (advance: boolean) => {
     const newValue = advance ? this.railPosition - 1 : this.railPosition + 1
-    if(newValue <= 0 && newValue > -8) this.railPosition = newValue
+    if(newValue <= 0 && newValue > -9) this.railPosition = newValue
     console.log("Posicionista:", this.railPosition)
+  }
+
+  public test = () => {
+    console.log("FOIS")
   }
 
 }
