357e0f3cd785b959cfb2a4755adefa3f5bcdb6a1
AlexandreUff <alexandre.magno.wrk@gmail.com>
2025-02-17 19:43:40 -0300

    feat: wip result view

--- src/app/pages/result/result.component.ts
+++ src/app/pages/result/result.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { UserDataType } from '../../types/UserData';
 import { CommonModule } from '@angular/common';
+import Result from '../../services/result';
+import { UserResult } from '../../types/Result';
 
 @Component({
   selector: 'app-result',
@@ -14,10 +16,10 @@ export class ResultComponent {
   constructor(private activedRoute: ActivatedRoute) {}
 
   public userData: UserDataType = {
-    weight: this.activedRoute.snapshot.params['weight'],
-    height: this.activedRoute.snapshot.params['height'],
-    age: this.activedRoute.snapshot.params['age'],
-    activity: this.activedRoute.snapshot.params['activity'],
+    weight: +this.activedRoute.snapshot.params['weight'],
+    height: +this.activedRoute.snapshot.params['height'],
+    age: +this.activedRoute.snapshot.params['age'],
+    activity: +this.activedRoute.snapshot.params['activity'],
     sex: this.activedRoute.snapshot.params['sex'],
     goal: this.activedRoute.snapshot.params['goal'],
   };
@@ -29,4 +31,29 @@ export class ResultComponent {
     this.userData.height &&
     this.userData.sex &&
     this.userData.weight;
+
+  public showResult = () => {
+    //console.log('RESULTISTA', Result(this.userData as UserDataType));
+
+    const {
+      caloricExpenditure,
+      carbo,
+      fat,
+      metabolicRate,
+      proteins,
+    }: UserResult = Result(this.userData as UserDataType);
+
+    console.log(
+      'Resulteiro',
+      caloricExpenditure,
+      carbo,
+      fat,
+      metabolicRate,
+      proteins
+    );
+  };
+
+  ngOnInit() {
+    this.showResult();
+  }
 }
