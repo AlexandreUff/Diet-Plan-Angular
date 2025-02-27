4f2e201dd6fa6c39447844aa127c87c71fb0172c
AlexandreUff <alexandre.magno.wrk@gmail.com>
2025-02-27 16:02:33 -0300

    refac: footer improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,13 @@ import Icons from '../assets/icons';
 import { DomSanitizer } from '@angular/platform-browser';
 import { LogoIconComponent } from './component/logo-icon/logo-icon.component';
 import { CarouselInputComponent } from './component/carousel-input/carousel-input.component';
+import ReleaseYear from './utils/ReleaseYear';
+import URLs from './utils/URLs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,
-            RouterOutlet,
-            IcoContentComponent,
-            LogoIconComponent,
-          ],
+  imports: [CommonModule, RouterOutlet, IcoContentComponent, LogoIconComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   /* styles: `
@@ -29,12 +27,24 @@ import { CarouselInputComponent } from './component/carousel-input/carousel-inpu
   ` */
 })
 export class AppComponent {
+  constructor(private sanitizer: DomSanitizer) {}
 
-  constructor(private sanitizer: DomSanitizer){}
+  public ShowCurrentYear: (year: string) => string = ReleaseYear;
+
+  public linkedinURL = URLs.LINKEDIN;
+  public githubURL = URLs.GITHUB;
 
   title = 'diet-plan';
-  logo = this.sanitizer.bypassSecurityTrustHtml(Icons.logo('width="55px" height="55px" fill="#baffb4"'))
-  linkedin = this.sanitizer.bypassSecurityTrustHtml(Icons.linkedin('width="30px" height="30px" fill="#baffb4"'))
-  github = this.sanitizer.bypassSecurityTrustHtml(Icons.github('width="30px" height="30px" fill="#baffb4"'))
-  instagram = this.sanitizer.bypassSecurityTrustHtml(Icons.instagram('width="30px" height="30px" fill="#baffb4"'))
+  logo = this.sanitizer.bypassSecurityTrustHtml(
+    Icons.logo('width="55px" height="55px" fill="#baffb4"')
+  );
+  linkedin = this.sanitizer.bypassSecurityTrustHtml(
+    Icons.linkedin('width="30px" height="30px" fill="#baffb4"')
+  );
+  github = this.sanitizer.bypassSecurityTrustHtml(
+    Icons.github('width="30px" height="30px" fill="#baffb4"')
+  );
+  instagram = this.sanitizer.bypassSecurityTrustHtml(
+    Icons.instagram('width="30px" height="30px" fill="#baffb4"')
+  );
 }
